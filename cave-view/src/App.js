edc7137a926f1ed5781bc90f4d32e996f8c1fb60
underarockphoto <underarockphoto@gmail.com>
2023-07-10 12:38:44 -0400

    interim commit with working GUI

--- cave-view/src/App.js
+++ cave-view/src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import map from './map.png';
 import './App.css';
+import React, {useState} from 'react'
+
+
+
+
+
 
 function App() {
+  const [size,setSize] = useState(1)
+  const setMapSize = (full) =>  document.getElementById("map").style.width = full?"auto":(size*100).toString()+"%"
+  
+  const zoomIn = () => {
+    setSize(size+.05)
+    setMapSize(false)
+  }
+
+  const zoomOut = () => {
+    setSize(size-.05)
+    setMapSize(false)
+  }
+
+  const zoomFit = () => {
+    setSize(1)
+    setMapSize(false)
+  }
+
+  const zoomFull = () => {
+    setMapSize(true)
+  }
+
+  const zoom = (e) => {
+  if (e.type === "click"){ 
+    if (e.ctrlKey)
+    {
+      zoomOut()
+    }else{
+     zoomIn()
+    }
+   
+  }
+
+  
+}
+const mouseOver = (e) => {
+    if (e.ctrlKey){
+      document.getElementById("map").style.cursor = "zoom-out"
+    }else{
+      document.getElementById("map").style.cursor = "zoom-in"
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="map"  >
+        <img src={map} id="map" className="App-map" alt="map" onClick={zoom} onMouseMove={mouseOver}/>
+        
+        </div>
+        <div className="controls">
+          <div className="zoom">
+             <button className="control" onClick={zoomIn}>+</button>      
+        <button className="control" onClick={zoomOut}>-</button>
+        <button className="control" onClick={zoomFit}>Fit</button>
+        <button className="control" onClick={zoomFull}>100%</button>
+          </div>
+       
+        </div>
     </div>
   );
 }
